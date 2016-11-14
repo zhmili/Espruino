565822023e9ef91a55e4172313d682a77f543307
Gordon Williams <gw@pur3.co.uk>
2016-11-14 18:05:34 +0000

    remove whitelist code

--- targets/nrf5x/bluetooth.c
+++ targets/nrf5x/bluetooth.c
@@ -48,9 +48,6 @@
 #include "fstorage.h"
 #include "ble_conn_state.h"
 static pm_peer_id_t m_peer_id;                              /**< Device reference handle to the current bonded central. */
-static pm_peer_id_t   m_whitelist_peers[BLE_GAP_WHITELIST_ADDR_MAX_COUNT];  /**< List of peers currently in the whitelist. */
-static uint32_t       m_whitelist_peer_cnt;                                 /**< Number of peers currently in the whitelist. */
-static bool           m_is_wl_changed;                                      /**< Indicates if the whitelist has been changed since last time it has been updated in the Peer Manager. */
 #endif
 
 // -----------------------------------------------------------------------------------
@@ -296,22 +293,6 @@ static void on_ble_evt(ble_evt_t * p_ble_evt)
         }
         if ((bleStatus & BLE_NEEDS_SOFTDEVICE_RESTART) && !jsble_has_connection())
           jsble_restart_softdevice();
-
-#if PEER_MANAGER_ENABLED
-        if (m_is_wl_changed) {
-            // The whitelist has been modified, update it in the Peer Manager.
-            err_code = pm_whitelist_set(m_whitelist_peers, m_whitelist_peer_cnt);
-            APP_ERROR_CHECK(err_code);
-
-            err_code = pm_device_identities_list_set(m_whitelist_peers, m_whitelist_peer_cnt);
-            if (err_code != NRF_ERROR_NOT_SUPPORTED)
-            {
-                APP_ERROR_CHECK(err_code);
-            }
-
-            m_is_wl_changed = false;
-        }
-#endif
 
         break;
 
@@ -677,26 +658,6 @@ static void on_adv_evt(ble_adv_evt_t ble_adv_evt) {
         case BLE_ADV_EVT_IDLE:
             break; //BLE_ADV_EVT_IDLE
 #if PEER_MANAGER_ENABLED
-        case BLE_ADV_EVT_WHITELIST_REQUEST:
-        {
-            ble_gap_addr_t whitelist_addrs[BLE_GAP_WHITELIST_ADDR_MAX_COUNT];
-            ble_gap_irk_t  whitelist_irks[BLE_GAP_WHITELIST_ADDR_MAX_COUNT];
-            uint32_t       addr_cnt = BLE_GAP_WHITELIST_ADDR_MAX_COUNT;
-            uint32_t       irk_cnt  = BLE_GAP_WHITELIST_ADDR_MAX_COUNT;
-
-            err_code = pm_whitelist_get(whitelist_addrs, &addr_cnt,
-                                        whitelist_irks,  &irk_cnt);
-            APP_ERROR_CHECK(err_code);
-            NRF_LOG_DEBUG("pm_whitelist_get returns %d addr in whitelist and %d irk whitelist\r\n",
-                           addr_cnt,
-                           irk_cnt);
-
-            // Apply the whitelist.
-            err_code = ble_advertising_whitelist_reply(whitelist_addrs, addr_cnt,
-                                                       whitelist_irks,  irk_cnt);
-            APP_ERROR_CHECK(err_code);
-        } break; //BLE_ADV_EVT_WHITELIST_REQUEST
-
         case BLE_ADV_EVT_PEER_ADDR_REQUEST:
         {
             pm_peer_data_bonding_t peer_bonding_data;
@@ -753,21 +714,6 @@ static void pm_evt_handler(pm_evt_t const * p_evt) {
             if (err_code != NRF_ERROR_BUSY)
             {
                 APP_ERROR_CHECK(err_code);
-            }
-            if (p_evt->params.conn_sec_succeeded.procedure == PM_LINK_SECURED_PROCEDURE_BONDING)
-            {
-                NRF_LOG_DEBUG("New Bond, add the peer to the whitelist if possible\r\n");
-                NRF_LOG_DEBUG("\tm_whitelist_peer_cnt %d, MAX_PEERS_WLIST %d\r\n",
-                               m_whitelist_peer_cnt + 1,
-                               BLE_GAP_WHITELIST_ADDR_MAX_COUNT);
-                if (m_whitelist_peer_cnt < BLE_GAP_WHITELIST_ADDR_MAX_COUNT)
-                {
-                    //bonded to a new peer, add it to the whitelist.
-                    m_whitelist_peers[m_whitelist_peer_cnt++] = m_peer_id;
-                    m_is_wl_changed = true;
-                }
-                //Note: This code will use the older bonded device in the white list and not add any newer bonded to it
-                //      You should check on what kind of white list policy your application should use.
             }
         } break;
 
@@ -987,30 +933,6 @@ uint32_t radio_notification_init(uint32_t irq_priority, uint8_t notification_typ
 
 #if PEER_MANAGER_ENABLED
 
-/**@brief Fetch the list of peer manager peer IDs.
- *
- * @param[inout] p_peers   The buffer where to store the list of peer IDs.
- * @param[inout] p_size    In: The size of the @p p_peers buffer.
- *                         Out: The number of peers copied in the buffer.
- */
-static void peer_list_get(pm_peer_id_t * p_peers, uint32_t * p_size)
-{
-    pm_peer_id_t peer_id;
-    uint32_t     peers_to_copy;
-
-    peers_to_copy = (*p_size < BLE_GAP_WHITELIST_ADDR_MAX_COUNT) ?
-                     *p_size : BLE_GAP_WHITELIST_ADDR_MAX_COUNT;
-
-    peer_id = pm_next_peer_id_get(PM_PEER_ID_INVALID);
-    *p_size = 0;
-
-    while ((peer_id != PM_PEER_ID_INVALID) && (peers_to_copy--))
-    {
-        p_peers[(*p_size)++] = peer_id;
-        peer_id = pm_next_peer_id_get(peer_id);
-    }
-}
-
 static void peer_manager_init(bool erase_bonds) {
   ble_gap_sec_params_t sec_param;
   ret_code_t           err_code;
@@ -1045,21 +967,6 @@ static void peer_manager_init(bool erase_bonds) {
 
   err_code = pm_register(pm_evt_handler);
   APP_ERROR_CHECK(err_code);
-
-  memset(m_whitelist_peers, PM_PEER_ID_INVALID, sizeof(m_whitelist_peers));
-  m_whitelist_peer_cnt = (sizeof(m_whitelist_peers) / sizeof(pm_peer_id_t));
-
-  peer_list_get(m_whitelist_peers, &m_whitelist_peer_cnt);
-
-  err_code = pm_whitelist_set(m_whitelist_peers, m_whitelist_peer_cnt);
-  APP_ERROR_CHECK(err_code);
-
-  // Setup the device identies list.
-  // Some SoftDevices do not support this feature.
-  err_code = pm_device_identities_list_set(m_whitelist_peers, m_whitelist_peer_cnt);
-  if (err_code != NRF_ERROR_NOT_SUPPORTED) {
-     APP_ERROR_CHECK(err_code);
-  }
 }
 #endif
 
